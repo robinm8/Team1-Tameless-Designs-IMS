7572d1835a4f68f6c3fd0e20da9617454f8711f5
Mark Robinson <robinm8@rpi.edu>
2019-04-04 03:36:01 -0400

    Create Dashboard Vue component

---
+++ src/components/Dashboard.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="thumbnail">
+        <div class="row">
+            <div class="col-md-6 col-md-offset-3 col-xs-10 col-xs-offset-1">
+                <div class="text-xs-center lighten-5 pa-5">Dashboard goes here</div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    import GridLoader from 'vue-spinner/src/GridLoader.vue'
+
+    export default {
+        components: {
+            GridLoader
+        }
+    }
+</script>
+
+<style>
+    .message {
+        color: white;
+        text-align: center;
+    }
+</style>
